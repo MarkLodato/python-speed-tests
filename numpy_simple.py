13870835625cce88b5b4764c47831342dd5fc45f
Mark Lodato <lodatom@gmail.com>
2011-08-27 14:40:37 -0400

    simplify numpy version

--- numpy_simple.py
+++ numpy_simple.py
@@ -2,31 +2,24 @@
 import numpy as np
 from comprehension import main
 
-def fft(x, y):
+def fft(x):
     N = len(x)
     if N <= 1:
-        y[:] = x[:]
-        return
+        return x
 
-    even = y[0::2]
-    odd  = y[1::2]
-    fft(x[0::2], even)
-    fft(x[1::2], odd)
+    even = fft(x[0::2])
+    odd =  fft(x[1::2])
 
-    M = len(odd)
-    expBase = -2j * np.pi / N
-    k = np.arange(M, dtype='complex')
-    rhs = np.exp(expBase*k) * odd
+    M = N / 2
+    rhs = np.exp(-2j*np.pi/N * np.arange(M)) * odd
     l = even + rhs
     r = even - rhs
-    y[:M] = l
-    y[M:] = r
+
+    return np.concatenate([l, r])
 
 
 def setup(N, count):
-    x = np.arange(N, dtype='complex')
-    y = np.empty_like(x)
-    return x, y
+    return np.arange(N, dtype='complex'),
 
 
 if __name__ == "__main__":
