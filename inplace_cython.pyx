a5f343958b1d0ed4d725e88204de9360e1c5b363
Mark Lodato <lodatom@gmail.com>
2011-08-28 12:36:27 -0400

    rewrite cython version in cython

--- inplace_cython.pyx
+++ inplace_cython.pyx
@@ -1,30 +1,42 @@
-#!/usr/bin/python
+from numpy cimport complex128_t as complex_t, ndarray
+from libc.math cimport M_PI
+cimport cython
+
 from common import main
-from cmath import exp, pi
+from cmath import exp
+import numpy as np
 
-
-def bitrev_shuffle(x):
-    N = len(x)
-    j = 0
-    for i in xrange(1, N):
+@cython.boundscheck(False)
+cdef bitrev_shuffle(ndarray[complex_t, ndim=1] x):
+    cdef int N = x.shape[0]
+    cdef int j = 0
+    cdef int i, b
+    cdef complex_t tmp
+    for i in range(1, N):
         b = N >> 1
         while j >= b:
             j -= b
             b >>= 1
         j += b
         if j > i:
-            x[i], x[j] = x[j], x[i]
+            tmp = x[i]
+            x[i] = x[j]
+            x[j] = tmp
 
 
-def fft_in_place(x):
-    N = len(x)
+@cython.boundscheck(False)
+cdef fft_in_place(ndarray[complex_t, ndim=1] x):
+    cdef int N = x.shape[0]
+    cdef int trans_size = 2
+    cdef complex_t wb, wb_step, a, b
+    cdef int i, j, t
+
     bitrev_shuffle(x)
-    trans_size = 2
     while trans_size <= N:
         wb = 1+0j
-        wb_step = exp(-2j * pi / trans_size)
-        for t in xrange(trans_size >> 1):
-            for trans in xrange(N / trans_size):
+        wb_step = exp(-2j * M_PI / trans_size)
+        for t in range(trans_size >> 1):
+            for trans in range(N / trans_size):
                 i = trans * trans_size + t
                 j = i + (trans_size >> 1)
                 a = x[i]
@@ -36,13 +48,13 @@ def fft_in_place(x):
 
 
 def fft(x):
-    x = list(x)
+    x = x.copy()
     fft_in_place(x)
     return x
 
 
 def setup(N):
-    return [complex(i) for i in range(N)],
+    return np.arange(N, dtype='complex'),
 
 
 if __name__ == "__main__":
